95a4bc0be79e3aa14f87cc683f8d8c33c5249f4b
Julián Eduardo Aguirre <aguirre.julian.eduardo@gmail.com>
2023-08-05 15:42:39 -0300

    Implemented ClientDTO without accountSet

--- src/main/java/com/mindhub/homebanking/controllers/ClientController.java
+++ src/main/java/com/mindhub/homebanking/controllers/ClientController.java
@@ -1,12 +1,15 @@
 package com.mindhub.homebanking.controllers;
 
+import com.mindhub.homebanking.dtos.ClientDTO;
 import com.mindhub.homebanking.models.Client;
 import com.mindhub.homebanking.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
@@ -14,7 +17,9 @@ public class ClientController {
     @Autowired
     private ClientRepository clientRepository;
     @RequestMapping("/clients")
-    public List<Client> getClients(){
-        return clientRepository.findAll();
+    public List<ClientDTO> getClients(){
+
+        return clientRepository.findAll().stream().map(client -> new ClientDTO(client)).collect(Collectors.toList());
+        //return clientRepository.findAll().stream().map(ClientDTO::new).collect(Collectors.toList());
     }
 }
