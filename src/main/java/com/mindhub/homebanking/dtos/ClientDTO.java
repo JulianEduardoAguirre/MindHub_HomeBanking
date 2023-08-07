99c2600bf139b9eb843c02913d4ddbdd25a42db8
Julián Eduardo Aguirre <aguirre.julian.eduardo@gmail.com>
2023-08-07 20:34:36 -0300

    Modified ClientDTO to return a Set of AccountDTO OK

--- src/main/java/com/mindhub/homebanking/dtos/ClientDTO.java
+++ src/main/java/com/mindhub/homebanking/dtos/ClientDTO.java
@@ -1,48 +1,38 @@
 package com.mindhub.homebanking.dtos;
 
-import com.mindhub.homebanking.models.Account;
 import com.mindhub.homebanking.models.Client;
-
-import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class ClientDTO {
     private long id;
     private String firstName;
     private String lastName;
     private String email;
-    private Set<Account> accounts;
+    //private Set<Account> accounts;
+    private Set<AccountDTO> accountsDTO;
 
     public ClientDTO(Client client) {
         this.id = client.getId();
         this.firstName = client.getFirstName();
         this.lastName = client.getLastName();
         this.email = client.getEmail();
-        Set<AccountDTO> accountsDTO = new HashSet<>();
-        this.accounts = client.getAccounts();
-        for (Account account: accounts) {
-            accountsDTO.add(new AccountDTO(account));
-        }
+        this.accountsDTO = client.getAccounts().stream().map(AccountDTO::new).collect(Collectors.toSet());
     }
-
     public long getId() {
         return id;
     }
-
     public String getFirstName() {
         return firstName;
     }
-
     public String getLastName() {
         return lastName;
     }
-
     public String getEmail() {
         return email;
     }
-
-    public Set<Account> getAccounts() {
-        return accounts;
+    public Set<AccountDTO> getAccountsDTO() {
+        return this.accountsDTO;
     }
 
 }
