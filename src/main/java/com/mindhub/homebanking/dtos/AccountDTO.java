ff1884dc69e1bfcc703d6b759cf430d2c0b5d49d
Julián Eduardo Aguirre <aguirre.julian.eduardo@gmail.com>
2023-08-08 20:21:50 -0300

    Coded Transaction class, enums and DTO. Modified AccountDTO to include TransactionDTOs. Als modified Account to include Transaction collection

--- src/main/java/com/mindhub/homebanking/dtos/AccountDTO.java
+++ src/main/java/com/mindhub/homebanking/dtos/AccountDTO.java
@@ -2,6 +2,8 @@ package com.mindhub.homebanking.dtos;
 
 import com.mindhub.homebanking.models.Account;
 import java.time.LocalDate;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class AccountDTO {
     private long id;
@@ -9,11 +11,16 @@ public class AccountDTO {
     private LocalDate date;
     private double balance;
 
+    private Set<TransactionDTO> transactions;
+
     public AccountDTO(Account account) {
         this.id = account.getId();
         this.number = account.getNumber();
         this.date = account.getCreationDate();
         this.balance = account.getBalance();
+        //this.accounts = client.getAccounts().stream().map(AccountDTO::new).collect(Collectors.toSet());
+
+        this.transactions = account.getTransactions().stream().map(TransactionDTO::new).collect(Collectors.toSet());
     }
 
     public long getId() {
@@ -31,5 +38,7 @@ public class AccountDTO {
     public double getBalance() {
         return balance;
     }
+
+    public Set<TransactionDTO> getTransactions() { return transactions;}
 
 }
