67f567848c47f60d8e71ffdd66e6e140d1c5e14f
Julián Eduardo Aguirre <aguirre.julian.eduardo@gmail.com>
2023-08-19 15:58:17 -0300

    Removed CORS not used

--- src/main/java/com/mindhub/homebanking/controllers/ClientController.java
+++ src/main/java/com/mindhub/homebanking/controllers/ClientController.java
@@ -4,7 +4,6 @@ import com.mindhub.homebanking.dtos.ClientDTO;
 import com.mindhub.homebanking.models.Client;
 import com.mindhub.homebanking.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +12,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-//@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping("/api")
 public class ClientController {
