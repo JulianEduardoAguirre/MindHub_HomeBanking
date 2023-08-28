c7cc0a0a4b3366ec48791812aabbbc4f7bf27ab9
Julián Eduardo Aguirre <aguirre.julian.eduardo@gmail.com>
2023-08-28 20:25:38 -0300

    Created add and sub in account balance - Creating transaction controller

---
+++ src/main/java/com/mindhub/homebanking/controllers/TransactionController.java
@@ -0,0 +1,88 @@
+package com.mindhub.homebanking.controllers;
+
+import com.mindhub.homebanking.dtos.ClientDTO;
+import com.mindhub.homebanking.models.Account;
+import com.mindhub.homebanking.models.Client;
+import com.mindhub.homebanking.models.Transaction;
+import com.mindhub.homebanking.models.TransactionType;
+import com.mindhub.homebanking.repositories.AccountRepository;
+import com.mindhub.homebanking.repositories.ClientRepository;
+import com.mindhub.homebanking.repositories.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import javax.transaction.Transactional;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api")
+public class TransactionController {
+
+    @Autowired
+    private TransactionRepository transactionRepository;
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Transactional
+    @RequestMapping(path = "/transactions", method = RequestMethod.POST)
+    public ResponseEntity<Object> makeTransaction (@RequestParam double amount,
+                                                   @RequestParam String description,
+                                                   @RequestParam String origin,
+                                                   @RequestParam String destiny,
+                                                   Authentication authentication) {
+
+        if (amount <= 0 || description.isEmpty() || origin.isEmpty() || destiny.isEmpty()) {
+            return new ResponseEntity<>("Missing data", HttpStatus.FORBIDDEN);
+        }
+
+        if (origin.equalsIgnoreCase(destiny)) {
+            return new ResponseEntity<>("Origin and destiny are equals", HttpStatus.FORBIDDEN);
+        }
+
+        Client clientOrigin = clientRepository.findByEmail(authentication.getName());
+
+        Optional<Account> originAccountOptional = Optional.ofNullable(accountRepository.findByNumber(origin));
+        Account originAccount;
+        if (originAccountOptional.isPresent()) {
+            originAccount = originAccountOptional.get();
+        } else {
+            return new ResponseEntity<>("Invalid origin account", HttpStatus.FORBIDDEN);
+        }
+
+        Optional<Account> destinyAccountOptional = Optional.ofNullable(accountRepository.findByNumber(destiny));
+        Account destinyAccount;
+        if (destinyAccountOptional.isPresent()) {
+            destinyAccount = destinyAccountOptional.get();
+        } else {
+            return new ResponseEntity<>("Invalid destiny account", HttpStatus.FORBIDDEN);
+        }
+
+        if (originAccount.getBalance() < amount){
+            return new ResponseEntity<>("Insuficient founds", HttpStatus.FORBIDDEN);
+        }
+
+        Transaction debit = new Transaction(TransactionType.DEBIT, -amount, description, LocalDateTime.now());
+        Transaction credit = new Transaction(TransactionType.CREDIT, amount, description, LocalDateTime.now());
+
+        originAccount.addTransaction(debit);
+        originAccount.substractAmount(amount);
+
+        destinyAccount.addTransaction(credit);
+        destinyAccount.addAmount(amount);
+
+        transactionRepository.save(debit);
+        transactionRepository.save(credit);
+
+        return new ResponseEntity<>("Transaction resolved", HttpStatus.CREATED);
+
+
+    }
+}
