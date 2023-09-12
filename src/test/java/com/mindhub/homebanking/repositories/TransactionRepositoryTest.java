6f728c29e3c4453a490afa05218c7b021ba35de4
Julián Eduardo Aguirre <aguirre.julian.eduardo@gmail.com>
2023-09-12 11:18:55 -0300

    Added transactionTest base code

--- src/test/java/com/mindhub/homebanking/repositories/TransactionRepositoryTest.java
+++ src/test/java/com/mindhub/homebanking/repositories/TransactionRepositoryTest.java
@@ -1,10 +1,38 @@
 package com.mindhub.homebanking.repositories;
 
+import com.mindhub.homebanking.models.Loan;
+import com.mindhub.homebanking.models.Transaction;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class TransactionRepositoryTest {
 
+
+    @Autowired
+    TransactionRepository transactionRepository;
+
+    @Test
+    public void existLoans(){
+
+        List<Transaction> transactions = transactionRepository.findAll();
+        assertThat(transactions,is(not(empty())));
+
+    }
+
+    @Test
+    public void existPersonalLoan(){
+
+        //List<Loan> loans = loanRepository.findAll();
+        assertThat(String.valueOf(true), true);
+
+    }
 }
